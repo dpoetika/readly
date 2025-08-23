75a567868de2ecc9f158339d765715b453b83926
dpoetika <yunuseakkayadp@gmail.com>
2025-08-23 04:50:55 +0300

    added docs in head endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,9 +9,16 @@ app.use(express.json())
 app.use("/books",bookRouter)
 app.use("/categories",categoryRouter)
 
-app.get("/", async(req, res) => {
-    res.send("server is alive");
+app.get("/", (req, res) => {
+  res.send(`usage:
+    https://readly-alpha.vercel.app/books → brings top books
+    https://readly-alpha.vercel.app/books/search?query={some string} → search
+    https://readly-alpha.vercel.app/books/:id → plain txt of book
+    https://readly-alpha.vercel.app/categories/ → list categories
+    https://readly-alpha.vercel.app/categories/:category_id → books in category
+  `);
 });
+
 
 
 /*
