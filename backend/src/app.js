ed5baf9d388efd400d42244152bda1b3a5465108
dpoetika <yunuseakkayadp@gmail.com>
2025-08-28 08:15:58 +0300

    adjust file

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,6 @@ import express from "express"
 import bookRouter from "./routes/books.routes.js"
 import categoryRouter from "./routes/category.routes.js"
 import limiter from "./middleware/rate_limiter.js"
-import config from "./config/config.js"
 
 const app = express()
 app.use(express.json())
@@ -26,6 +25,5 @@ app.get("/", (req, res) => {
 app.listen(config.PORT, () => {
   console.log("http://localhost:3000/books/top\nhttp://localhost:3000/books/search/?query=sa");
 });
-
 */
 export default app;
