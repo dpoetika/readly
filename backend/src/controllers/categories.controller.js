f5b2eae30b07152b3c849f2d33e944cd93dd749e
dpoetika <yunuseakkayadp@gmail.com>
2025-08-28 07:49:09 +0300

    handle errors

--- backend/src/controllers/categories.controller.js
+++ backend/src/controllers/categories.controller.js
@@ -4,36 +4,40 @@ import config from "../config/config.js";
 
 export async function get_categories() {
     console.log(config.GET_CATEGORIES_URI)
-    const { data } = await axios.get(config.GET_CATEGORIES_URI);      
-    const $ = cheerio.load(data);              
+    const { data } = await axios.get(config.GET_CATEGORIES_URI);
+    const $ = cheerio.load(data);
 
     const results = [];
 
     $(".bookshelves .book-list ul li a").each((i, el) => {
-        const href = $(el).attr("href");       
-        const text = $(el).text().trim();  
-        results.push({ id:href.replace("/ebooks/bookshelf/",""),title:text });
+        const href = $(el).attr("href");
+        const text = $(el).text().trim();
+        results.push({ id: href.replace("/ebooks/bookshelf/", ""), title: text });
     });
 
-    return {message:"succes","data":results};
+    return { message: "succes", "data": results };
 }
 
 
 export async function get_category_books(category_id) {
-    const objects = []; 
-    for (let index = 0; index < 4; index++) {
-        
-        const { data } = await axios.get(`${config.GET_CATEGORY_BOOKS}/${category_id}/?start_index=${String((index*25)+1)}`);      
-        const $ = cheerio.load(data);              
+    try {
+        const objects = [];
+        for (let index = 0; index < 4; index++) {
 
-        $("li.booklink").each((i, el) => {
-            const aTag = $(el).find("a.link");
-            const id = aTag.attr("href").replace("/ebooks/","");  
-            const img = aTag.find("img.cover-thumb").attr("src"); 
-            const title = aTag.find("span.title").text().trim();
-            const author = aTag.find("span.subtitle").text().trim();
-            objects.push({ id, img:"https://www.gutenberg.org" + img.replace("small","medium"), title, author})
-        });
+            const { data } = await axios.get(`${config.GET_CATEGORY_BOOKS}/${category_id}/?start_index=${String((index * 25) + 1)}`);
+            const $ = cheerio.load(data);
+
+            $("li.booklink").each((i, el) => {
+                const aTag = $(el).find("a.link");
+                const id = aTag.attr("href").replace("/ebooks/", "");
+                const img = aTag.find("img.cover-thumb").attr("src");
+                const title = aTag.find("span.title").text().trim();
+                const author = aTag.find("span.subtitle").text().trim();
+                objects.push({ id, img: "https://www.gutenberg.org" + img.replace("small", "medium"), title, author })
+            });
+        }
+        return { message: "succes", "data": objects };
+    } catch (error) {
+        return { message: "failed", error: error.message || error };
     }
-    return {message:"succes","data":objects};
 }
--- backend/src/controllers/get_book.controller.js
+++ backend/src/controllers/get_book.controller.js
@@ -2,27 +2,24 @@ import axios from "axios";
 import config from "../config/config.js";
 
 export async function get_book(id) {
-    const { data } = await axios.get(`${config.GET_BOOK_URI}/${id}/pg${id}.txt`);
+    try {
+        const { data } = await axios.get(`${config.GET_BOOK_URI}/${id}/pg${id}.txt`);
 
-    const startTag = "*** START OF THE PROJECT GUTENBERG EBOOK";
-    const endTag = "*** END OF THE PROJECT GUTENBERG EBOOK";
+        const startTag = "*** START OF THE PROJECT GUTENBERG EBOOK";
+        const endTag = "*** END OF THE PROJECT GUTENBERG EBOOK";
 
-    // Başlangıç indexi
-    const startIndex = data.indexOf(startTag);
-    const endIndex = data.indexOf(endTag);
+        const startIndex = data.indexOf(startTag);
+        const endIndex = data.indexOf(endTag);
 
-    if (startIndex === -1 || endIndex === -1) {
-        throw new Error("Kitap başlangıç veya bitiş etiketi bulunamadı.");
+        if (startIndex === -1 || endIndex === -1) {
+            throw new Error("Kitap başlangıç veya bitiş etiketi bulunamadı.");
+        }
+
+        let content = data.slice(startIndex + data.substring(startIndex).indexOf("\n") + 1, endIndex);
+
+        return { message: "succes", "data": content.trim() };
+
+    } catch (error) {
+        return { message: "failed", error: error.message || error };
     }
-
-    // İçeriği kes
-    let content = data.slice(startIndex + data.substring(startIndex).indexOf("\n") + 1, endIndex);
-
-    // Satır sonlarını normalize et
-    //content = content.replace(/\r\n/g, "\n").replace(/\r/g, "\n");
-
-    // Gereksiz boş satırları temizle
-    //content = content.replace(/\n\s*\n/g, "\n\n");
-
-    return {message:"succes","data":content.trim()};
 }
